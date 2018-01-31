db151737df7478d568be0e986930e7d93d71f4aa
Tav Hafner <tav.hafner@gmail.com>
2018-01-31 14:34:27 -0700

    finished nesting

--- nesting.js
+++ nesting.js
@@ -51,6 +51,18 @@ var employees = [
 */
 
 //Code Here
+function employeeUpdater() {
+  for (var i=0 ; i < employees.length; i++) {
+    if (employees[i].firstName === 'Theo') {
+      employees.splice(i, 1);
+      i--;
+    } else if (employees[i].firstName === 'Lorie') {
+      employees[i].department = 'HR';
+    }
+  }
+  return employees;
+
+}
 
 
 
@@ -69,6 +81,17 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 */
 
 //Code Here
+function removeDuplicates(){
+  for(let i = 0;i<workplaceAccidents.length;i++){
+    for(let j = i+1;j<workplaceAccidents.length;j++){
+      if(workplaceAccidents[i] === workplaceAccidents[j] ){
+        workplaceAccidents.splice(j,1)
+        j--
+      }
+    }
+  }
+  return workplaceAccidents
+}
 
 
 
@@ -97,8 +120,9 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1]
+var fluffy2ndFriend = cat.catFriends[1].name;
+
 
 
 
@@ -139,6 +163,11 @@ var myCar = {
 */
 
 //Code Here
+function recordCleaner(){
+  for(let i = 0;i<myCar.accidents.length;i++){
+    myCar.accidents[i].atFaultForAccident = false;
+  }
+}
 
 
 
@@ -158,5 +187,18 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
 */
 
 //Code Here
-
+function looper() {
+  for (let i=0; i < numsArr.length; i++) {
+    for (let j=0; j < numsArr[i].length; j++) {
+      if (numsArr[i][j] % 2 === 0) {
+        //numsArr[i].splice(j, 1, 'even');
+        numsArr[i][j] = 'even';
+      } else {
+        //numsArr[i].splice(j, 1, 'odd');
+        numsArr[i][j] = 'odd';
+      }
+    }
+  }
+  return numsArr;
+}
 
